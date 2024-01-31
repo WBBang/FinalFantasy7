80969b7497f5d7d3c2dcb45c7a89e31d307db6ce
LazarusKim <devlazarus729@gmail.com>
2024-01-31 15:23:59 +0900

    1.31 Add:
    Create Folder, ActorClass : Boss, Missile

---
+++ FF7/Source/FF7/Private/KEC/FinalBoss.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KEC/FinalBoss.h"
+#include "KEC/Missile.h"
+#include "../../../../../../../Source/Runtime/Engine/Classes/Components/BoxComponent.h"
+#include "../../../../../../../Source/Runtime/Engine/Classes/Components/StaticMeshComponent.h"
+#include "../../../../../../../Source/Runtime/Engine/Classes/Components/ArrowComponent.h"
+
+// Sets default values
+AFinalBoss::AFinalBoss()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("boxComp"));
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("meshComp"));
+	firePosition = CreateDefaultSubobject<UArrowComponent>(TEXT("firePosition"));
+
+	SetRootComponent(boxComp);
+	meshComp->SetupAttachment(RootComponent);
+	firePosition->SetupAttachment(RootComponent);
+	missile = AMissile::StaticClass();
+	World = GetWorld();
+}
+
+// Called when the game starts or when spawned
+void AFinalBoss::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFinalBoss::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFinalBoss::LaunchMissile()
+{
+	FVector launchLocation = firePosition->GetComponentLocation();
+	//AMissile* spawnedMissile = World->SpawnActor<AMissile>(missile, launchLocation);
+}
+
---
+++ FF7/Source/FF7/Public/KEC/FinalBoss.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FinalBoss.generated.h"
+
+UCLASS()
+class FF7_API AFinalBoss : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFinalBoss();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UStaticMeshComponent* meshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UBoxComponent* boxComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UArrowComponent* firePosition;
+
+	void LaunchMissile();
+
+	TSubclassOf<class Amissile> missile;
+	class UWorld* World;
+};
